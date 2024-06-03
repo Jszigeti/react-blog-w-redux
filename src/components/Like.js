838eb5f6ca47da7e2d0b015a018950c6ea623e4f
Jszigeti <j.szigeti1993@gmail.com>
2024-06-03 21:29:40 +0200

    Adding post likes feature

--- src/components/Like.js
+++ src/components/Like.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addPostLike } from "../actions/posts.actions";
+import { addUserLike } from "../actions/user.actions";
 
 const Like = ({ post }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.userReducer);
+
+  const handleLike = () => {
+    const postData = {
+      title: post.title,
+      content: post.content,
+      author: post.author,
+      id: post.id,
+      likes: post.likes + 1,
+    };
+
+    const userData = {
+      pseudo: user.pseudo,
+      likes: user.likes + 1,
+      age: user.age,
+      id: user.id,
+    };
+
+    dispatch(addPostLike(postData));
+    dispatch(addUserLike(userData));
+  };
+
   return (
     <div>
-      <img src="./icons/clap.png" className="clap" alt="clap" />
+      <img
+        src="./icons/clap.png"
+        className="clap"
+        alt="clap"
+        onClick={() => handleLike()}
+      />
       <span>{post.likes}</span>
     </div>
   );
